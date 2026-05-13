8d316bb5f18ad9edcfde2a1ce6d320b3b529ddf3
Kirin <kirinlyraota.work@gmail.com>
2026-05-13 19:55:39 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LP1Semana10
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
