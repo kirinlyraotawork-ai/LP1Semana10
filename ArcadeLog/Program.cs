2d9d89d25eeb8ef33f8996d0ee049e58b465eaff
Kirin <kirinlyraota.work@gmail.com>
2026-05-14 16:59:50 +0100

    Second commit

--- ArcadeLog/Program.cs
+++ ArcadeLog/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
 
 namespace ArcadeLog
 {
@@ -11,16 +13,87 @@ namespace ArcadeLog
         private static void Main(string[] args)
         {
             // Lê o Ficheiro e Cria os Scores
+            using StreamReader sr = new (args[0]);
+            /*while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                    break;
+                sw.WriteLine(input);
+            }*/
+
+
+
             List<Score> scores = new List<Score>();
             // CÓDIGO AQUI
 
             // Ordena os Scores
+            scores.Sort();
+
+
             // CÓDIGO AQUI
 
             // Agrupa por Medalha e Imprime (Gold → Silver → Bronze)
+            foreach (string medal in new[] { "Gold", "Silver", "Bronze" })
+            {
+                foreach (Score score in scores)
+                {
+                    if (score.Medal == medal)
+                    {
+                        Console.WriteLine($"{score.Name} [{score.Medal}]: {score.Points}");
+                    }
+                }
+            }
+
+          // else if (Score.points >= 4000)
+          //     7 {
+          //7/          return "Silver";
+           //     }
+            //    else
+             //   {
+            //        return "Bronze";
+           //     }
+
+         //   if (Score s in scores == )
+          //  {
+                
+          //  }
+          //  foreach (Score score in scores)
+           // {
+           //     Console.WriteLine($"{score.Name} [{score.Medal.value}]: {score.Points}");
+           // }
+            
+             /*Program prog = new Program();
+            // Start the program instance
+            prog.Start();*/
+
+
             // Escreve a lista no ficheiro ranking.txt
             // CÓDIGO AQUI
+            //using StreamWriter sw = new (args[0]);
+            /*while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "")
+                    break;
+                sw.WriteLine(input);
+            }*/
+            /*string fileName = args[0];
+             Queue<string> queue = new();
 
+        while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "")
+                    break;
+
+                queue.Enqueue(input);
+
+            }
+        File.WriteAllLines( fileName, queue);
+        }*/
+            File.WriteAllLines("ranking.txt", scores.ConvertAll(s => $"{s.Name} [{s.Medal}]: {s.Points}"));
             Console.WriteLine("Ranking guardado em 'ranking.txt'.");
 
             // Ordena por Nome e Escreve em alpha.txt
