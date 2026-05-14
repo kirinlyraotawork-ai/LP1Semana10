ce451d83e31c952f5876dadea33d85b95dd138b3
Kirin <kirinlyraota.work@gmail.com>
2026-05-14 14:38:07 +0100

    Show another message

---
+++ IntCollections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntCollections
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Insere os valores {1, 10, -30, 10, -5} em cada uma das coleção, pela ordem indicada
+            List<int> lista = new() {1,10,-30,10,-5};
+            Stack<int> stack = new();
+            stack.Push(1);
+            stack.Push(10);
+            stack.Push(-30);
+            stack.Push(10);
+            stack.Push(-5);
+            foreach (int i in stack)
+            {
+                Console.Write(i);
+            }
+
+            int[] array = {1,10,-30,10,-5};
+            Queue<int> queue = new();
+            HashSet<int> hashset = new();
+            foreach (int i in array)
+            {
+                queue.Enqueue(i);
+                hashset.Add(i);
+                
+            }
+            Console.WriteLine(string.Join(",",queue));
+            
+        }
+    }
+}
---
+++ MeuTeste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeuTeste
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>();
+            dict["first"] = 10;
+            dict["second"] = 20;
+            dict["third"] = 30;
+
+            Console.WriteLine(string.Join("; ", dict.Select(entry => $"{entry.Key}: {entry.Value}")));
+            // Output:
+            // first: 10; second: 20; third: 30
+        }
+    }
+}
